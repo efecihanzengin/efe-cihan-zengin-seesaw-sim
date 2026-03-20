419c405ac673e423a8eef7c2eb01911630379b7f
Efe Cihan Zengin <efecihanzengin@gmail.com>
2026-03-20 14:54:27 +0300

    fix: plank point relativity calculation changed

--- script.js
+++ script.js
@@ -2,20 +2,16 @@ const plank = document.querySelector(".plank");
 const objects = [];
 
 plank.addEventListener("click", function (event) {
-  const rect = plank.getBoundingClientRect(); // plankin ekranin solundan uzakligi(rect.left)
-  const clickX = event.clientX - rect.left; // plankin en solundan itibaren ne kadar uzaga tikladigi
-  const distanceFromCenter = clickX - rect.width / 2; // formul icin gerekli merkezden ne kadar uzaga tikladigi
+  const click = event.offsetX; // plankin solundan uzakligi
+  const distanceFromCenter = click - 200; // formul icin gerekli merkezden ne kadar uzaga tikladigi
   const weight = Math.floor(Math.random() * 10) + 1;
-
-  console.log(rect);
-  console.log(clickX);
-  console.log(distanceFromCenter);
 
   objects.push({
     weight: weight,
     distance: distanceFromCenter, // negatif = sol, pozitif = sag
   });
-  createNewWeightObject(clickX, weight);
+
+  createNewWeightObject(click, weight);
   updateSeesaw();
 });
 
